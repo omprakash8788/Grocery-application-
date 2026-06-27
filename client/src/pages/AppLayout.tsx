13fdbe118d0a0f4e1a6acfeaddf01c4315e7730f
omprakash8788 <omprakashkumar552@gmail.com>
2026-06-27 16:35:31 +0530

    [feat]: adding banner section

--- client/src/pages/AppLayout.tsx
+++ client/src/pages/AppLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Banner from "../components/Banner";
 
 const AppLayout = () => {
   return (
     <>
-      <p>banner</p>
+      {/* <p>banner</p> */}
+      <Banner/>
       <p>navbar</p>
       <main className="min-h-screen">
         <Outlet />
