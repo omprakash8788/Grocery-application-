fe91d01a8a54fb6f0daeb7b55a94943e1ced7bf7
omprakash8788 <omprakashkumar552@gmail.com>
2026-07-12 17:34:30 +0530

    final code pusblish for complete full stack UI with user admin, and delivery partner dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,15 @@ import MyOrder from "./pages/MyOrder";
 import OrderTracking from "./pages/OrderTracking";
 import Addresses from "./pages/Addresses";
 import ProtectedRoute from "./components/ProtectedRoute";
+import AdminLayout from "./pages/admin/AdminLayout";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminProductForm from "./pages/admin/AdminProductForm";
+import AdminProducts from "./pages/admin/AdminProducts";
+import AdminOrders from "./pages/admin/AdminOrders";
+import AdminDeliveryPartners from "./pages/admin/AdminDeliveryPartners";
+import DeliveryLogin from "./pages/delivery/DeliveryLogin";
+import DeliveryLayout from "./pages/delivery/DeliveryLayout";
+import DeliveryDashboard from "./pages/delivery/DeliveryDashboard";
 
 const App = () => {
   return (
@@ -30,21 +39,36 @@ const App = () => {
       />
       <Routes>
         {/* Auth pages - No Navbar/Footer */}
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/login" element={<Login />} />
         {/* Main pages with navbar and footer */}
-        <Route path="/" element={<AppLayout/>}>
-        <Route index element={<Home/>}/>
-        <Route path="/products" element={<Products/>}/>
-        <Route path="/products/:id" element={<ProductPage/>}/>
-        <Route path="/search" element={<SearchResutls/>}/>
-        <Route path="/deals" element={<FlashDeals/>}/>
-        {/* Protected route section */}
-        <Route element={<ProtectedRoute/>}>
-          <Route path="checkout" element={<Checkout/>}/>
-          <Route path="orders" element={<MyOrder/>}/>
-          <Route path="orders/:id" element={<OrderTracking/>}/>
-          <Route path="addresses" element={<Addresses/>}/>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductPage />} />
+          <Route path="/search" element={<SearchResutls />} />
+          <Route path="/deals" element={<FlashDeals />} />
+          {/* Protected route section */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="orders" element={<MyOrder />} />
+            <Route path="orders/:id" element={<OrderTracking />} />
+            <Route path="addresses" element={<Addresses />} />
+          </Route>
         </Route>
+        {/* Admin Pages */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="products/new" element={<AdminProductForm />} />
+          <Route path="products/:id/edit" element={<AdminProductForm />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="delivery-partners" element={<AdminDeliveryPartners />} />
+        </Route>
+
+        {/* Delivery partner pages */}
+        <Route path="/delivery/login" element={<DeliveryLogin />} />
+        <Route path="/delivery" element={<DeliveryLayout />}>
+          <Route index element={<DeliveryDashboard />} />
         </Route>
       </Routes>
     </>
