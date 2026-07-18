8b6c2d237533fb6451d65610bf8fd35414ade548
omprakash8788 <omprakashkumar552@gmail.com>
2026-07-18 14:40:46 +0530

    error handler

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config"
-import express from "express"
+import express, { NextFunction, Request, Response } from "express"
 
 import cors from "cors"
 import authRouter from "./routes/authRoutes.js";
@@ -11,12 +11,19 @@ app.use(express.json())
 
 const port = process.env.PORT || 3000
 
-app.get('/', (re, res)=>{
+app.get('/', (req:Request, res:Response)=>{
     res.send("Server is Live")
 })
 
 //23 Register phase
 app.use('/api/auth', authRouter)
+
+// 24 Error handling - This function catch any error in all routes and controller function
+app.use((error:any ,req:Request, res:Response, next:NextFunction)=>{
+  console.log(error(error))
+  res.status(500).json({message:error.message})
+})
+
 
 app.listen(port,()=>{
     console.log(`Server is running at http://localhost:${port}`)
